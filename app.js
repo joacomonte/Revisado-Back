2124ec90d5e2045ba01a1792aee4dc61c1bdf29a
lucasburgosm <montesanolucas@gmail.com>
2022-12-07 18:13:57 -0300

    add cors port

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 const products = require('./routes/productsTasks')
 const connectDB = require('./db/connect')
 require('dotenv').config()
+const cors = require('cors')
 
 app.use(express.json());
+app.use(cors())
 
 //hola
 app.get('/', (req, res) => {
