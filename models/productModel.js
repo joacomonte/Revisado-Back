2ff6fd706e64d46a75ea09d78a242cf612241326
Joaquin Montesano <montesano1994@gmail.com>
2022-12-12 12:46:30 -0300

    holas

--- models/productModel.js
+++ models/productModel.js
@@ -1,61 +1,71 @@
 const mongoose = require('mongoose');
 
 // Create a product schema
-// Create a product schema
 const productSchema = new mongoose.Schema({
-    idProduct : {
+    idProduct : {                 
         type : Number, 
         required: [true, "must have idProduct"],
         trim: true,
     },
-    brand : {
+    brand : {                 
         type : String, 
         required: [true, "must have brand"],
         trim: true,
         maxlength: [20, "brand cant have more than 20 caracters"]
     },
-    modelName : {
+    modelName : {                 
         type : String, 
         required: [true, "must have modelName"],
         trim: true,
         maxlength: [20, "modelName cant have more than 20 caracters"]
     },
-    modelNumber : {
+    modelNumber : {                 
         type : String, 
         required: [true, "must have  modelNumber"],
         trim: true,
         maxlength: [30, "modelNumber cant have more than 20 caracters"]
     },
-    caracts : {
+    caracts : {                 
         type : String, 
         required: [true, "must have caracts"],
         trim: true,
         maxlength: [30, "caracts  cant have more than 20 caracters"]
     },
-    nameToDisplay : {
+    nameToDisplay : {                 
         type : String, 
         required: [true, "must have nameToDisplay"],
         trim: true,
         maxlength: [30, "nameToDisplay cant have more than 20 caracters"]
     },
-    price : {
+    price : {                 
         type : Number, 
         required: [true, "must have  price "],
         trim: true,
         maxlength: [30, "price cant have more than 20 caracters"]
     },
-    color : {
+    color : {                 
         type : String, 
         required: [true, "must have color"],
         trim: true,
         maxlength: [30, "spects cant have more than 20 caracters"]
     },
-    details : {
+    details : {                 
         type : String, 
         required: [true, "must have details"],
         trim: true,
         maxlength: [30, "spects cant have more than 20 caracters"]
     },
+});
+
+const usersSchema = new mongoose.Schema({
+    nameDisplay : {                 
+        type : String, 
+        required: [true, "must have nameDisplay"],
+        maxlength: [20, "name cant have more than 20 caracters"]
+    },
+    brand: String,
+    model: String,
+    price: Number,
 });
 
 
@@ -64,5 +74,3 @@ const Products = mongoose.model('Products', productSchema)
 const User = mongoose.model('Users', usersSchema)
 
 module.exports = {Products, User};
-
-
