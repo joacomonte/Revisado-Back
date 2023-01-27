6f6be705532859f1b14eae350588a165baa1e3f5
Joaquin Montesano <montesano1994@gmail.com>
2023-01-26 23:01:17 -0300

    req pelado 2

--- middleware/authenticator.js
+++ middleware/authenticator.js
@@ -7,7 +7,7 @@ const auth = async (req,res, next) => {
   const authCookie = req.cookies.token;
   console.log(req.cookies)
   console.log(req.cookie)
-  console.log(req)
+  Consolé.log("este es el req" + req)
 
   // if(!authHeader || !authHeader.startsWith('Bearer ')){
   //     throw new errorUnauthenticated('Authentication Invalid bad auto')
@@ -15,7 +15,7 @@ const auth = async (req,res, next) => {
  if(!authCookie){
   console.log(req.cookies)
   console.log(req.cookie)
-  console.log(req)
+  Consolé.log("este es el req" + req)
 
    throw new errorUnauthenticated('Cookie missing')
  }
