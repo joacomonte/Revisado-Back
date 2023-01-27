191834ec250eb305f846c208b015fcafcd128e9e
Joaquin Montesano <montesano1994@gmail.com>
2023-01-27 20:02:02 -0300

    funciona?

--- app.js
+++ app.js
@@ -24,12 +24,12 @@ const errorHanddler = require('./middleware/errorHanddler')
 app.get('/', (req, res) => { res.send('Revisado BACK END') })
 app.use('/api/products/all', notAuthRouter );
 app.use('/api/auth', userLogin);
-app.use(function(req, res, next) {
-   res.header("Access-Control-Allow-Origin", "*");
-   res.header("Access-Control-Allow-Credentials", "true");
-   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-   next();
-});
+// app.use(function(req, res, next) {
+//    res.header("Access-Control-Allow-Origin", "*");
+//    res.header("Access-Control-Allow-Credentials", "true");
+//    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//    next();
+// });
 app.use('/api/products', auth, productsRouter);
 
 // app.use('/api/products', auth, productsRouter);
